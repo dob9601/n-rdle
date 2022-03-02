95ecd09f3678c20af0eae06d095e810ea2744456
Daniel O'Brien <dob9601@gmail.com>
2022-03-02 18:37:04 +0000

    Icons, better frontpage

--- src/App.tsx
+++ src/App.tsx
@@ -25,17 +25,19 @@ export const DictContext = React.createContext<string[]>([])
 function App() {
     if (window.location.hostname.split(".").length <= 2) {
         return (
-            <>
-                <h1>Ordle</h1>
-                <p>Ordle is wordle, but procedurally generated!</p>
-                <h2>How does it work?</h2>
-                <ul>
-                    <li>Navigate to a numeric subdomain to get that number of wordles</li>
-                    <li>Wordles above size 500 aren{"'"}t recommended unless you have a beefy machine</li>
-                    <li>Adding /{"<"}number{">"} to the end of the url will customise the word length. For example, <a href="https://w.ordle.xyz/8">w.ordle.xyz/8</a></li>
-                    <li>Certain numeral prefixes are also supported, for example <a href="https://w.ordle.xyz">w.ordle.xyz</a>, <a href="https://qu.ordle.xyz">qu.ordle.xyz</a>, <a href="https://cent.ordle.xyz">cent.ordle.xyz</a></li>
-                </ul>
-            </>
+            <div className="DescriptionContainer">
+                <div className="Description">
+                    <h1>Ordle</h1>
+                    <p className="tagline">Ordle is wordle, but procedurally generated!</p>
+                    <h2>How does it work?</h2>
+                    <ul>
+                        <li>Navigate to a numeric subdomain to get that number of wordles</li>
+                        <li>Wordles above size 500 aren{"'"}t recommended unless you have a beefy machine</li>
+                        <li>Adding /{"<"}number{">"} to the end of the url will customise the word length. For example, <a href="https://w.ordle.xyz/8">w.ordle.xyz/8</a></li>
+                        <li>Certain numeral prefixes are also supported, for example <a href="https://w.ordle.xyz">w.ordle.xyz</a>, <a href="https://qu.ordle.xyz">qu.ordle.xyz</a>, <a href="https://cent.ordle.xyz">cent.ordle.xyz</a></li>
+                    </ul>
+                </div>
+            </div>
         )
     } else {
         return (
