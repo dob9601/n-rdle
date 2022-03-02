7dbfcfc12b0d2245e9b77449e8f4befee6537fb1
Daniel O'Brien <dob9601@gmail.com>
2022-03-02 00:20:43 +0000

    Fix url

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ function App() {
                     <li>Navigate to a numeric subdomain to get that number of wordles</li>
                     <li>Wordles above size 500 aren{"'"}t recommended unless you have a beefy machine</li>
                     <li>Adding /{"<"}number{">"} to the end of the url will customise the word length. For example, <a href="https://w.ordle.xyz/8">w.ordle.xyz/8</a></li>
-                    <li>Certain numeral prefixes are also supported, for example <a href="https://w.ordle.xyz">w.ordle.xyz</a>, <a href="https://qu.ordle.xyz">qu.ordle.xyz</a>, <a href="https://">cent.ordle.xyz</a></li>
+                    <li>Certain numeral prefixes are also supported, for example <a href="https://w.ordle.xyz">w.ordle.xyz</a>, <a href="https://qu.ordle.xyz">qu.ordle.xyz</a>, <a href="https://cent.ordle.xyz">cent.ordle.xyz</a></li>
                 </ul>
             </>
         )
